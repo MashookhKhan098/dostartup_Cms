8e18894cfec08265aed93dcda995264362a459ca
Adi3182004 <adi3182004@gmail.com>
2025-10-06 18:42:31 +0530

    imageadded

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
-
-
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Services from "./components/Services";
 import Updates from "./components/Updates";
 import Footer from "./components/Footer";
-import Popularsearches from './components/PopularSearches';
-import Partnership from './components/Partnership';
-import Ourstory from './components/Ourstory';
+import Popularsearches from "./components/PopularSearches";
+import Partnership from "./components/Partnership";
+import Ourstory from "./components/Ourstory";
 
 export default function Home() {
   return (
